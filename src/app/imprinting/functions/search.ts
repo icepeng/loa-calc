813ad94156acd0d366c62b9b509ccc32d5fc32db
ggrn <gyareon124@gmail.com>
2021-10-23 02:00:46 +0900

    refactor global searchScript property

--- src/app/imprinting/functions/search.ts
+++ src/app/imprinting/functions/search.ts
@@ -1,17 +1,22 @@
 import { SearchOption } from './type';
+declare global {
+  interface Window {
+    searchScript: Function;
+  }
+}
 
 export async function getSearchScript(
   toSearch: Record<string, number>[],
   accMap: Record<string, SearchOption>
 ) {
-  if(!(globalThis as any).searchScript) {
+  if(!window.searchScript) {
     await fetch('assets/js/searchScript.js').then(res => res.text()).then(scr => {
       const script = document.createElement('script');
       document.body.appendChild(script);
       script.innerHTML = scr;
     });
   }
-  const searchScriptRegex = /^function.+?\{(.*)\}$/gis.exec(((globalThis as any).searchScript as Function).toString());
+  const searchScriptRegex = /^function.+?\{(.*)\}$/gis.exec((window.searchScript).toString());
   if (!searchScriptRegex || searchScriptRegex.length < 2) {
     throw new Error('Could not find search function');
   }

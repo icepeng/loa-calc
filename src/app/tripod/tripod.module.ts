dc377421ca9fc9e7d687893b3e87b729bdd1699d
ha <typ0@naver.com>
2021-11-03 02:18:33 +0900

    build: increase budget limit

--- src/app/tripod/tripod.module.ts
+++ src/app/tripod/tripod.module.ts
@@ -2,29 +2,31 @@ import { ClipboardModule } from '@angular/cdk/clipboard';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table';
 import { TripodFormComponent } from './components/tripod-form.component';
+import { TripodItemViewComponent } from './components/tripod-item-view.component';
+import { TripodTableComponent } from './components/tripod-table.component';
 import { TripodComponent } from './containers/tripod.component';
 import { TripodRoutingModule } from './tripod-routing.module';
-import { TripodTableComponent } from './components/tripod-table.component';
-import { TripodItemViewComponent } from './components/tripod-item-view.component';
 
 @NgModule({
-  declarations: [TripodComponent, TripodFormComponent, TripodTableComponent, TripodItemViewComponent],
+  declarations: [
+    TripodComponent,
+    TripodFormComponent,
+    TripodTableComponent,
+    TripodItemViewComponent,
+  ],
   imports: [
     CommonModule,
     TripodRoutingModule,
@@ -39,11 +41,8 @@ import { TripodItemViewComponent } from './components/tripod-item-view.component
     MatTableModule,
     MatSnackBarModule,
     MatPaginatorModule,
-    MatAutocompleteModule,
     MatProgressSpinnerModule,
-    MatIconModule,
     MatMenuModule,
-    MatRadioModule,
     MatSlideToggleModule,
     ClipboardModule,
   ],

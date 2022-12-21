c10328c18d796218c5dc6ed3690fb18ca34d75d5
icepeng <typ0@naver.com>
2022-12-22 01:21:35 +0900

    refactor getPrice

--- src/app/market-price.service.ts
+++ src/app/market-price.service.ts
@@ -24,74 +24,42 @@ export class MarketPriceService {
     }
 
     const items = this.data.items;
+
+    const getPrice = (name: string) =>
+      items.find((x) => x.Name === name)?.[priceType] ?? 0;
     const obj = {
       파편: Math.min(
-        items.find((x) => x.Name === '명예의 파편 주머니(소)')![priceType] /
-          500,
-        items.find((x) => x.Name === '명예의 파편 주머니(중)')![priceType] /
-          1000,
-        items.find((x) => x.Name === '명예의 파편 주머니(대)')![priceType] /
-          1500
+        getPrice('명예의 파편 주머니(소)') / 500,
+        getPrice('명예의 파편 주머니(중)') / 1000,
+        getPrice('명예의 파편 주머니(대)') / 1500
       ),
-      하급오레하: items.find((x) => x.Name === '하급 오레하 융화 재료')![
-        priceType
-      ],
-      중급오레하: items.find((x) => x.Name === '중급 오레하 융화 재료')![
-        priceType
-      ],
-      상급오레하: items.find((x) => x.Name === '상급 오레하 융화 재료')![
-        priceType
-      ],
-      최상급오레하: items.find((x) => x.Name === '최상급 오레하 융화 재료')![
-        priceType
-      ],
-      명돌: items.find((x) => x.Name === '명예의 돌파석')![priceType],
-      위명돌: items.find((x) => x.Name === '위대한 명예의 돌파석')![priceType],
-      경명돌: items.find((x) => x.Name === '경이로운 명예의 돌파석')![
-        priceType
-      ],
-      찬명돌: items.find((x) => x.Name === '찬란한 명예의 돌파석')![priceType],
-      수결: items.find((x) => x.Name === '수호석 결정')![priceType] / 10,
-      파결: items.find((x) => x.Name === '파괴석 결정')![priceType] / 10,
-      수호강석: items.find((x) => x.Name === '수호강석')![priceType] / 10,
-      파괴강석: items.find((x) => x.Name === '파괴강석')![priceType] / 10,
-      정제된수호강석:
-        items.find((x) => x.Name === '정제된 수호강석')![priceType] / 10,
-      정제된파괴강석:
-        items.find((x) => x.Name === '정제된 파괴강석')![priceType] / 10,
-      은총: items.find((x) => x.Name === '태양의 은총')![priceType],
-      축복: items.find((x) => x.Name === '태양의 축복')![priceType],
-      가호: items.find((x) => x.Name === '태양의 가호')![priceType],
-      재봉술기본: items.find((x) => x.Name === '재봉술 : 수선 기본')![
-        priceType
-      ],
-      재봉술응용: items.find((x) => x.Name === '재봉술 : 수선 응용')![
-        priceType
-      ],
-      재봉술심화: items.find((x) => x.Name === '재봉술 : 수선 심화')![
-        priceType
-      ],
-      재봉술숙련: items.find((x) => x.Name === '재봉술 : 수선 숙련')![
-        priceType
-      ],
-      재봉술특화: items.find((x) => x.Name === '재봉술 : 수선 특화')![
-        priceType
-      ],
-      야금술기본: items.find((x) => x.Name === '야금술 : 단조 기본')![
-        priceType
-      ],
-      야금술응용: items.find((x) => x.Name === '야금술 : 단조 응용')![
-        priceType
-      ],
-      야금술심화: items.find((x) => x.Name === '야금술 : 단조 심화')![
-        priceType
-      ],
-      야금술숙련: items.find((x) => x.Name === '야금술 : 단조 숙련')![
-        priceType
-      ],
-      야금술특화: items.find((x) => x.Name === '야금술 : 단조 특화')![
-        priceType
-      ],
+      하급오레하: getPrice('하급 오레하 융화 재료'),
+      중급오레하: getPrice('중급 오레하 융화 재료'),
+      상급오레하: getPrice('상급 오레하 융화 재료'),
+      최상급오레하: getPrice('최상급 오레하 융화 재료'),
+      명돌: getPrice('명예의 돌파석'),
+      위명돌: getPrice('위대한 명예의 돌파석'),
+      경명돌: getPrice('경이로운 명예의 돌파석'),
+      찬명돌: getPrice('찬란한 명예의 돌파석'),
+      수결: getPrice('수호석 결정') / 10,
+      파결: getPrice('파괴석 결정') / 10,
+      수호강석: getPrice('수호강석') / 10,
+      파괴강석: getPrice('파괴강석') / 10,
+      정제된수호강석: getPrice('정제된 수호강석') / 10,
+      정제된파괴강석: getPrice('정제된 파괴강석') / 10,
+      은총: getPrice('태양의 은총'),
+      축복: getPrice('태양의 축복'),
+      가호: getPrice('태양의 가호'),
+      재봉술기본: getPrice('재봉술 : 수선 기본'),
+      재봉술응용: getPrice('재봉술 : 수선 응용'),
+      재봉술심화: getPrice('재봉술 : 수선 심화'),
+      재봉술숙련: getPrice('재봉술 : 수선 숙련'),
+      재봉술특화: getPrice('재봉술 : 수선 특화'),
+      야금술기본: getPrice('야금술 : 단조 기본'),
+      야금술응용: getPrice('야금술 : 단조 응용'),
+      야금술심화: getPrice('야금술 : 단조 심화'),
+      야금술숙련: getPrice('야금술 : 단조 숙련'),
+      야금술특화: getPrice('야금술 : 단조 특화'),
     };
 
     return Object.fromEntries(

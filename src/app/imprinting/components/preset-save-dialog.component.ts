2504c94ca849ab69fff3d9d4b91b34e6d671e3eb
icepeng <typ0@naver.com>
2023-11-04 22:28:01 +0900

    fix(imprinting): prevent duplicate name

--- src/app/imprinting/components/preset-save-dialog.component.ts
+++ src/app/imprinting/components/preset-save-dialog.component.ts
@@ -1,17 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { map, startWith } from 'rxjs/operators';
+import { imprintingPresetToken } from '../functions/const';
+import { Preset } from '../functions/type';
 
 @Component({
   selector: 'app-preset-save-dialog',
   templateUrl: './preset-save-dialog.component.html',
 })
-export class PresetSaveDialogComponent {
-  presetNameControl = new FormControl('', Validators.required);
+export class PresetSaveDialogComponent implements OnInit {
+  presets: Preset[] = [];
+  presetNameControl = new FormControl('', [
+    Validators.required,
+    (control) => {
+      return this.presets.some((preset) => preset.name === control.value)
+        ? { duplicate: true }
+        : null;
+    },
+  ]);
 
   constructor(private dialogRef: MatDialogRef<PresetSaveDialogComponent>) {}
 
+  ngOnInit() {
+    this.presets = JSON.parse(
+      localStorage.getItem(imprintingPresetToken) || '[]'
+    );
+  }
+
   onSubmit() {
+    if (this.presetNameControl.invalid) {
+      return;
+    }
     this.dialogRef.close({
       name: this.presetNameControl.value,
     });
